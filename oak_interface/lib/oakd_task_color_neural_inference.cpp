5c496d00084e34d0f8513397bff8991c38d8e0fb
RPS98 <57761265+RPS98@users.noreply.github.com>
2021-06-30 12:07:08 +0200

    Uso de tryget en la inferencia en RGB

--- oak_interface/lib/oakd_task_color_neural_inference.cpp
+++ oak_interface/lib/oakd_task_color_neural_inference.cpp
@@ -62,88 +62,89 @@ void OakDTaskColorNeuralInference::run(std::vector<std::shared_ptr<dai::DataOutp
             color_info_pub.publish(color_CameraInfo);
         }
         
-        det = streams_queue[queue_index.inx_detections_color]->get<dai::SpatialImgDetections>();
+        det = streams_queue[queue_index.inx_detections_color]->tryGet<dai::SpatialImgDetections>();
 
-        counter_++;
-        auto currentTime = steady_clock::now();
-        auto elapsed = duration_cast<duration<float>>(currentTime - startTime);
-        if(elapsed > seconds(1)) {
-            fps_ = counter_ / elapsed.count();
-            counter_ = 0;
-            startTime = currentTime;
-        }
-
-        cv::Mat frame = OakDUtils::getCvFrame(color_frame);
-
-        for(const auto& d : det->detections) {
-            labelIndex = d.label;
-            labelStr = std::to_string(labelIndex);
-            if(labelIndex < label_map.size()) {
-                labelStr = label_map[labelIndex];
+        if(!(det == nullptr)){
+            counter_++;
+            auto currentTime = steady_clock::now();
+            auto elapsed = duration_cast<duration<float>>(currentTime - startTime);
+            if(elapsed > seconds(1)) {
+                fps_ = counter_ / elapsed.count();
+                counter_ = 0;
+                startTime = currentTime;
             }
-            // Process just the objects that will be sent
-            if (labelStr == "diningtable" || labelStr == "tvmonitor" || labelStr == "bottle" || labelStr == "chair"){
-                // Get rid of outliers
-                if ((float)d.spatialCoordinates.z/1000.0 > 0.7 && (float)d.spatialCoordinates.z/1000.0 < 3.0){
 
-                    x1 = d.xmin * frame.cols;
-                    y1 = d.ymin * frame.rows;
-                    x2 = d.xmax * frame.cols;
-                    y2 = d.ymax * frame.rows;
+            cv::Mat frame = OakDUtils::getCvFrame(color_frame);
+
+            for(const auto& d : det->detections) {
+                labelIndex = d.label;
+                labelStr = std::to_string(labelIndex);
+                if(labelIndex < label_map.size()) {
+                    labelStr = label_map[labelIndex];
+                }
+                // Process just the objects that will be sent
+                if (labelStr == "diningtable" || labelStr == "tvmonitor" || labelStr == "bottle" || labelStr == "chair"){
+                    // Get rid of outliers
+                    if ((float)d.spatialCoordinates.z/1000.0 > 0.7 && (float)d.spatialCoordinates.z/1000.0 < 3.0){
+
+                        x1 = d.xmin * frame.cols;
+                        y1 = d.ymin * frame.rows;
+                        x2 = d.xmax * frame.cols;
+                        y2 = d.ymax * frame.rows;
 
 
-                    cv::putText(frame, labelStr, cv::Point(x1 + 10, y1 + 20), cv::FONT_HERSHEY_TRIPLEX, 0.5, color_);
+                        cv::putText(frame, labelStr, cv::Point(x1 + 10, y1 + 20), cv::FONT_HERSHEY_TRIPLEX, 0.5, color_);
 
-                    confStr.str("");
-                    confStr << std::fixed << std::setprecision(2) << d.confidence * 100;
-                    cv::putText(frame, confStr.str(), cv::Point(x1 + 10, y1 + 35), cv::FONT_HERSHEY_TRIPLEX, 0.5, color_);
+                        confStr.str("");
+                        confStr << std::fixed << std::setprecision(2) << d.confidence * 100;
+                        cv::putText(frame, confStr.str(), cv::Point(x1 + 10, y1 + 35), cv::FONT_HERSHEY_TRIPLEX, 0.5, color_);
 
-                    depthX.str("");
-                    depthX << "X: " << std::fixed << std::setprecision(3) << (float)d.spatialCoordinates.x/1000 << " m";
-                    cv::putText(frame, depthX.str(), cv::Point(x1 + 10, y1 + 50), cv::FONT_HERSHEY_TRIPLEX, 0.5, color_);
-                    
-                    depthY.str("");
-                    depthY << "Y: " << std::fixed << std::setprecision(3) << (float)d.spatialCoordinates.y/1000 << " m";
-                    cv::putText(frame, depthY.str(), cv::Point(x1 + 10, y1 + 65), cv::FONT_HERSHEY_TRIPLEX, 0.5, color_);
-                    
-                    depthZ.str("");
-                    depthZ << "Z: " << std::fixed << std::setprecision(3) << (float)d.spatialCoordinates.z/1000 << " m";
-                    cv::putText(frame, depthZ.str(), cv::Point(x1 + 10, y1 + 80), cv::FONT_HERSHEY_TRIPLEX, 0.5, color_);
+                        depthX.str("");
+                        depthX << "X: " << std::fixed << std::setprecision(3) << (float)d.spatialCoordinates.x/1000 << " m";
+                        cv::putText(frame, depthX.str(), cv::Point(x1 + 10, y1 + 50), cv::FONT_HERSHEY_TRIPLEX, 0.5, color_);
+                        
+                        depthY.str("");
+                        depthY << "Y: " << std::fixed << std::setprecision(3) << (float)d.spatialCoordinates.y/1000 << " m";
+                        cv::putText(frame, depthY.str(), cv::Point(x1 + 10, y1 + 65), cv::FONT_HERSHEY_TRIPLEX, 0.5, color_);
+                        
+                        depthZ.str("");
+                        depthZ << "Z: " << std::fixed << std::setprecision(3) << (float)d.spatialCoordinates.z/1000 << " m";
+                        cv::putText(frame, depthZ.str(), cv::Point(x1 + 10, y1 + 80), cv::FONT_HERSHEY_TRIPLEX, 0.5, color_);
 
-                    cv::rectangle(frame, cv::Rect(cv::Point(x1, y1), cv::Point(x2, y2)), color_, cv::FONT_HERSHEY_SIMPLEX);
+                        cv::rectangle(frame, cv::Rect(cv::Point(x1, y1), cv::Point(x2, y2)), color_, cv::FONT_HERSHEY_SIMPLEX);
 
-                    bbox.Class = (std::string)labelStr;
-                    bbox.probability = (float)d.confidence;
-                    bbox.xmin = (int)x1;
-                    bbox.ymin = (int)y1;
-                    bbox.xmax = (int)x2;
-                    bbox.ymax = (int)y2;
-                    bbox.depth = (float)d.spatialCoordinates.z/1000;
-                    bbox.x_centroid = (float)d.spatialCoordinates.x/1000;
-                    bbox.y_centroid = (float)d.spatialCoordinates.y/1000;
-                    msg.header.stamp = header.stamp;
+                        bbox.Class = (std::string)labelStr;
+                        bbox.probability = (float)d.confidence;
+                        bbox.xmin = (int)x1;
+                        bbox.ymin = (int)y1;
+                        bbox.xmax = (int)x2;
+                        bbox.ymax = (int)y2;
+                        bbox.depth = (float)d.spatialCoordinates.z/1000;
+                        bbox.x_centroid = (float)d.spatialCoordinates.x/1000;
+                        bbox.y_centroid = (float)d.spatialCoordinates.y/1000;
+                        msg.header.stamp = header.stamp;
 
-                    msg.bounding_boxes.push_back(bbox);
+                        msg.bounding_boxes.push_back(bbox);
+                    }
                 }
             }
+
+            // Print fps
+            fpsStr.str("");
+            fpsStr << std::fixed << std::setprecision(2) << fps_;
+            cv::putText(frame, fpsStr.str(), cv::Point(2, color_frame->getHeight() - 4), cv::FONT_HERSHEY_TRIPLEX, 0.4, color_);
+
+            // From cv::Mat to cv_bridge::CvImage 
+            cv_bridge::CvImage frame_cv_bridge = cv_bridge::CvImage(header,sensor_msgs::image_encodings::BGR8, frame);
+            // From cv_bridge::CvImage to sensor_msgs::Image
+            frame_cv_bridge.toImageMsg(frame_msg);
+            // Publishing the image with detections in the topic "/detections/image"
+            frame_msg.header.stamp = header.stamp;
+            image_detections_pub.publish(frame_msg);
+            // Publishing the info of detections in the topic "/detections/bounding_boxes"
+            detections_pub.publish(msg); 
+            msg.bounding_boxes.clear();
         }
-
-        // Print fps
-        fpsStr.str("");
-        fpsStr << std::fixed << std::setprecision(2) << fps_;
-        cv::putText(frame, fpsStr.str(), cv::Point(2, color_frame->getHeight() - 4), cv::FONT_HERSHEY_TRIPLEX, 0.4, color_);
-
-        // From cv::Mat to cv_bridge::CvImage 
-        cv_bridge::CvImage frame_cv_bridge = cv_bridge::CvImage(header,sensor_msgs::image_encodings::BGR8, frame);
-        // From cv_bridge::CvImage to sensor_msgs::Image
-        frame_cv_bridge.toImageMsg(frame_msg);
-        // Publishing the image with detections in the topic "/detections/image"
-        frame_msg.header.stamp = header.stamp;
-        image_detections_pub.publish(frame_msg);
-        // Publishing the info of detections in the topic "/detections/bounding_boxes"
-        detections_pub.publish(msg); 
-        msg.bounding_boxes.clear();
-        
     }
     
 };
