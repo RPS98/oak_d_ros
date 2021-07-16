82aad946a16440043522d6a440fd66b539613d83
RPS98 <57761265+RPS98@users.noreply.github.com>
2021-07-16 15:54:08 +0200

    Corrección del commit anterior, juntando las versiones

--- oak_interface/include/oak_interface/oakd_task_color_neural_inference.hpp
+++ oak_interface/include/oak_interface/oakd_task_color_neural_inference.hpp
@@ -20,8 +20,6 @@ private:
     // DETECTIONS
     static const std::vector<std::string> label_map;
 
-    std::shared_ptr<dai::ImgFrame> imgFrame;
-    std::shared_ptr<dai::ImgFrame> depth;
     std::shared_ptr<dai::SpatialImgDetections> det;
     std::shared_ptr<dai::SpatialLocationCalculatorConfig> boundingBoxMapping;
 
@@ -45,6 +43,40 @@ private:
     float fps_;
     cv::Scalar color_;
 
+
+
+    // COLOR IMAGE PUBLISHER
+    // Cameras info
+    std::unique_ptr<camera_info_manager::CameraInfoManager> color_camInfoManager;
+    sensor_msgs::CameraInfo color_CameraInfo;
+    // Image
+    sensor_msgs::Image color_image_msg;
+    std::shared_ptr<dai::ImgFrame> color_frame;
+    // Publishers
+    ros::Publisher color_pub;
+    ros::Publisher color_info_pub;
+
+    // Use interleaved
+    bool publish_color = false;
+    bool use_interleaved = false;
+    void get_RosMsg(std::shared_ptr<dai::ImgFrame> color_frame,
+                    sensor_msgs::Image &color_image_msg,
+                    std_msgs::Header header);
+
+
+
+    // RUN VARIABLES
+    int labelIndex;
+    std::string labelStr;
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+    std::stringstream confStr;
+    std::stringstream depthX;
+    std::stringstream depthY;
+    std::stringstream depthZ;
+    std::stringstream fpsStr;
 };
 
 #endif
